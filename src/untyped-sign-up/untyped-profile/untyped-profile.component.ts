cdb52e94f259f2dc9efde217f2104ffe3d65b884
Joseph R Gravlin <jgravlin@vermeer.com>
2023-10-30 15:55:52 -0400

    Init `Untyped{Profile,SignUp}Component`

---
+++ src/untyped-sign-up/untyped-profile/untyped-profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-untyped-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './untyped-profile.component.html',
+  styleUrls: ['./untyped-profile.component.css']
+})
+export class UntypedProfileComponent {
+
+}
---
+++ src/untyped-sign-up/untyped-sign-up.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-untyped-sign-up',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './untyped-sign-up.component.html',
+  styleUrls: ['./untyped-sign-up.component.css']
+})
+export class UntypedSignUpComponent {
+
+}
